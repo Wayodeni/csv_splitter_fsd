64282184aa2ef33f1974dff506d8d0cbfdf3cb28
FoxyGDFD <mic.panfil0v@gmail.com>
2024-06-25 22:57:48 +0300

    fix selected variant in SectionsMenu

--- src/widgets/SectionsMenu.tsx
+++ src/widgets/SectionsMenu.tsx
@@ -2,15 +2,15 @@ import { TabList, TabListProvider } from "simplify-dev";
 import { useSectionStore } from "../entities/section/store";
 import { GetUsersButton } from "../features/GetUsersButton";
 
-
 export const SectionsMenu = () => {
     const { sections } = useSectionStore();
+
     if (!sections?.length) return null;
     return <>
-        <TabListProvider defaultIndex={sections[0]}>
-                <TabList selectedVariant="secondary">
-                {sections?.map((section) => <GetUsersButton section={section} key={section} />)}
-                </TabList>
-            </TabListProvider>
+        <TabListProvider defaultSelectedId={sections[0]}>
+            <TabList selectedVariant="secondary">
+            {sections?.map((section) => <GetUsersButton section={section} />)}
+            </TabList>
+        </TabListProvider>
     </>
 }
